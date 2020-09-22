88b7332a151e0151628ba88ff9bbad63ac0f1355
Eanson.S.Yan <eanson.s.yan@newegg.com>
2020-09-22 16:17:48 +0800

    a

--- ch42/reflect_test.go
+++ ch42/reflect_test.go
@@ -2,6 +2,7 @@ package ch42
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"net/url"
 	"reflect"
 	"strconv"
@@ -22,6 +23,15 @@ type LoginForm struct {
 	RadioButtonList1 string
 	Button1          string
 }
+type scoreForm struct {
+	__VIEWSTATE string
+	// 学年
+	ddlXN string
+	// 学期
+	ddlXQ string
+	// 安学期查询
+	Button1 string
+}
 
 func newLoginForm(username string, password string, checkcode string) *LoginForm {
 	return &LoginForm{
@@ -32,28 +42,25 @@ func newLoginForm(username string, password string, checkcode string) *LoginForm
 		RadioButtonList1: "学生",
 	}
 }
-func (lf *LoginForm) addToRequestForm(form url.Values) {
-	getValue := reflect.ValueOf(lf).Elem()
-	getType := reflect.TypeOf(lf).Elem()
+func addToRequestForm(form interface{}) {
+	getValue := reflect.ValueOf(form).Elem()
+	getType := reflect.TypeOf(form).Elem()
 	// 反射添加到map
 	// 反射添加进条件中
 	params := make([]string, 4)
 	for i := 0; i < getValue.NumField(); i++ {
 		str := getType.Field(i).Name + "=" + getValue.Field(i).String()
 		params = append(params, str)
-		// _ = writer.WriteField(getType.Field(i).Name, getValue.Field(i).String())
 	}
 	ret := url.QueryEscape(strings.Join(params, "&"))
 	fmt.Println(ret)
 }
 
 func Test(t *testing.T) {
-	lf := newLoginForm("1", "2", "3")
-	var form url.Values = make(url.Values)
-	lf.addToRequestForm(form)
-	for key, value := range form {
-		t.Log(key, value)
+	var scoreForm *scoreForm = &scoreForm{
+		ddlXN: "1",
 	}
+	addToRequestForm(scoreForm)
 }
 
 func TestUrlEncode(t *testing.T) {
@@ -62,4 +69,30 @@ func TestUrlEncode(t *testing.T) {
 	sText := "中文"
 	textQuoted := strconv.QuoteToASCII(sText)
 	t.Log(textQuoted)
+}
+
+func TestTTTT(t *testing.T) {
+	str := "大家好：12345"
+	t.Log(strings.Split(str, "：")[1])
+}
+
+func TestGoquery(t *testing.T) {
+	html := `<body>
+
+				<div>DIV1</div>
+				<div class="name">DIV2</div>
+				<span>SPAN</span>
+
+			</body>
+			`
+
+	dom, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dom.Find("div[class=name]").Each(func(i int, selection *goquery.Selection) {
+		fmt.Println(selection.Text())
+	})
+	t.Log(dom.Find("div[class=name]").Text())
 }
--- ch42/scanner/scanner.go
+++ ch42/scanner/scanner.go
@@ -1,41 +1,39 @@
 package main
 
 import (
+	"time"
 	// "bufio"
 	"fmt"
 	// "os"
+	"sync"
 )
 
+func getText() chan string {
+	var str string
+	ch := make(chan string)
+	fmt.Scanln(&str)
+	ch <- str
+	return ch
+}
+
 func main() {
-	// // 创建一个map 指定key为string类型 val为int类型
-	// counts := make(map[string]int)
-	// // 从标准输入流中接收输入数据
-	// input := bufio.NewScanner(os.Stdin)
-
-	fmt.Printf("Please type in something:\n")
-
-	// // 逐行扫描
-	// for input.Scan() {
-	// 	line := input.Text()
-
-	// 	// // 输入bye时 结束
-	// 	// if line == "bye" {
-	// 	// 	break
-	// 	// }
-
-	// 	// 更新key对应的val 新key对应的val是默认0值
-	// 	counts[line]++
-	// 	break
-	// }
-
-	// // 遍历map统计数据
-	// for line, n := range counts {
-	// 	fmt.Printf("%d : %s\n", n, line)
-	// }
-	var text string
-	if text == "" {
-		fmt.Println("sajsaio")
+	time.Sleep(time.Second * 5)
+	// fmt.Println(<-getText())
+	var str string
+	fmt.Scanln(&str)
+	fmt.Println(str)
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			wg.Add(1)
+			go func() {
+				mutex.Lock()
+				fmt.Println(i, j)
+				mutex.Unlock()
+				wg.Done()
+			}()
+		}
 	}
-	fmt.Scanln(&text)
-	fmt.Println(text)
+	wg.Wait()
 }
