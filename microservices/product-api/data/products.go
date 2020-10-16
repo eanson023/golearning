24b14147b8049ef4e27803e0d9569fcac14f9bca
eanson <yanxiansen1999@gmail.com>
2020-10-16 23:04:25 +0800

    build restful online coffee system

--- microservices/product-api/data/products.go
+++ microservices/product-api/data/products.go
@@ -1,6 +1,9 @@
 package data
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"time"
 )
 
@@ -14,10 +17,6 @@ type Product struct {
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"` //使用'-'忽略该字段
-}
-
-func GetProducts() []*Product {
-	return producList
 }
 
 var producList = []*Product{
@@ -39,4 +38,57 @@ var producList = []*Product{
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
+}
+
+// FromJSON deserialize
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
+
+type Products []*Product
+
+// ToJSON 封装json数据 将其写入到writer中
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+// GetProducts 模仿数据库 获取数据
+func GetProducts() Products {
+	return producList
+}
+
+// AddProduct 添加商品
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	p.CreatedOn = time.Now().UTC().String()
+	p.UpdatedOn = time.Now().UTC().String()
+	producList = append(producList, p)
+}
+
+// UpdateProduct 更新商品
+func UpdateProduct(p *Product) error {
+	fp, pos, err := findProduct(p.ID)
+	if err != nil {
+		return err
+	}
+	producList[pos] = fp
+	return nil
+}
+
+var ErrorProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for k, p := range producList {
+		if p.ID == id {
+			return p, k, nil
+		}
+	}
+	return nil, -1, ErrorProductNotFound
+}
+
+func getNextID() int {
+	lp := producList[len(producList)-1]
+	return lp.ID + 1
 }
--- microservices/product-api/handlers/products.go
+++ microservices/product-api/handlers/products.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
-	"encoding/json"
+	"regexp"
+	"strconv"
+	// "encoding/json"
 	"github.com/eanson023/golearning/microservices/product-api/data"
 	"log"
 	"net/http"
@@ -16,11 +18,66 @@ func NewProductsHandler(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		p.getProducts(rw, req)
+	case http.MethodPost:
+		p.addProduct(rw, req)
+	case http.MethodPut:
+		r := regexp.MustCompile(`/([0-9]+)`)
+		g := r.FindAllStringSubmatch(req.URL.Path, -1)
+
+		if len(g) != 1 || len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, _ := strconv.Atoi(idString)
+		p.l.Println("got id", id)
+		p.updateProduct(id, rw, req)
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+	p.l.Println("Hanlde GET Products")
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+	// d, err := json.Marshal(lp)
+	// 封装一些实现细节
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Hanlde POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 		return
 	}
-	rw.Write(d)
+	p.l.Printf("Prod: %#v\n", prod)
+	data.AddProduct(prod)
+}
+
+// 更新产品 我们希望能从URI中得到ID
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, req *http.Request) {
+	p.l.Println("Hanlde PUT Product")
+	prod := &data.Product{}
+	err := prod.FromJSON(req.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+	prod.ID = id
+	p.l.Printf("Prod: %#v\n", prod)
+	err = data.UpdateProduct(prod)
+	if err == data.ErrorProductNotFound {
+		http.Error(rw, "product not found", http.StatusNotFound)
+	}
 }
