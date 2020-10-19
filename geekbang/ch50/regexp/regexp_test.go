4691a62a679b036f5c05a79dd67a9274621310cc
eanson <yanxiansen1999@gmail.com>
2020-10-19 20:38:39 +0800

    regexp

--- geekbang/ch50/regexp/regexp_test.go
+++ geekbang/ch50/regexp/regexp_test.go
@@ -1,8 +1,12 @@
 package regexp
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
 	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -60,4 +64,78 @@ func TestRegexp4(t *testing.T) {
 	// 匹配出1
 	b, _ = regexp.MatchString(`\d+?`, s)
 	assert.True(t, b)
+}
+
+func TestRegexp5(t *testing.T) {
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		fmt.Println("http get error.")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("http read error")
+		return
+	}
+
+	src := string(body)
+
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	fmt.Println(strings.TrimSpace(src))
+}
+
+func TestRegexp6(t *testing.T) {
+	a := "I am learning Go language"
+	re, _ := regexp.Compile("[a-z]{2,4}")
+
+	// 查找符合正则的第一个
+	one := re.Find([]byte(a))
+	assert.Equal(t, "am", string(one))
+
+	// 查找符合正则的所有slice,n小于0表示返回全部符合的字符串，不然就是返回指定的长度
+	all := re.FindAllString(a, -1)
+	// [am lear ning lang uage]
+	t.Log(all)
+
+	// 查找符合条件的index位置,开始位置和结束位置
+	index := re.FindIndex([]byte(a))
+	assert.Equal(t, []int{2, 4}, index)
+
+	//查找符合条件的所有的index位置，n同上
+	allindex := re.FindAllStringIndex(a, -1)
+	// [[2 4] [5 9] [9 13] [17 21] [21 25]]
+	t.Log(allindex)
+}
+
+// 查找Submatch,返回数组，第一个元素是匹配的全部元素，第二个元素是第一个()里面的，第三个是第二个()里面的
+//下面的输出第一个元素是"am learning Go language"
+//第二个元素是" learning Go "，注意包含空格的输出
+//第三个元素是"uage"
+func TestRegexp7(t *testing.T) {
+	a := "I am learning Go language"
+	re2, _ := regexp.Compile("am(.*)lang(.*)")
+	submatch := re2.FindSubmatch([]byte(a))
+	t.Log(submatch)
+	for _, v := range submatch {
+		t.Log(string(v))
+	}
 }
